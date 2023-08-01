85888671dbaa3b07e70bfdbe9b4cb194dd53d28c
crissu91 <cristinaf.stefan@gmail.com>
2023-08-01 13:22:56 +0100

    add controller for books

--- api/controllers/books.controller.js
+++ api/controllers/books.controller.js
@@ -1,7 +1,16 @@
- const {selectBooks} = require('../models/books.model')
+ const {selectBooks, selectBooksById} = require('../models/books.model')
 
 exports.getBooks = (req, res, next) => {
     selectBooks().then((books) => {
         res.status(200).send({books})
     })
+}
+
+exports.getBookById = (req, res, next) => {
+    const {id} = req.params
+    selectBooksById(id)
+    .then((book) => {
+        res.status(200).send({book})
+    })
+    .catch(next)
 }
